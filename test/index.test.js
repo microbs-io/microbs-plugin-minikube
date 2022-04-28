ae705361b3254a1c1bdec23c360485abddca64a0
Dave Moore <davemoore.cc@gmail.com>
2022-04-28 16:24:19 -0400

    Added more tests

--- test/index.test.js
+++ test/index.test.js
@@ -10,8 +10,24 @@ const { utils } = require('@microbs.io/core')
 
 describe('microbs plugin standards', () => {
 
+  test('plugin.json exists', () => {
+    const json = utils.loadJson(path.join(process.cwd(), 'plugin.json'))
+    expect(json).toBeTruthy()
+  })
+
   test('plugin.json declares "type" as "kubernetes"', () => {
     const json = utils.loadJson(path.join(process.cwd(), 'plugin.json'))
     expect(json.type).toBe('kubernetes')
+  })
+
+  test('package-lock.json exists', () => {
+    const json = utils.loadJson(path.join(process.cwd(), 'package-lock.json'))
+    expect(json).toBeTruthy()
+  })
+
+  test('package.json and package-lock.json have same version', () => {
+    const pkg = utils.loadJson(path.join(process.cwd(), 'package.json'))
+    const pkgLock = utils.loadJson(path.join(process.cwd(), 'package-lock.json'))
+    expect(pkg.version).toBe(pkgLock.version)
   })
 })
