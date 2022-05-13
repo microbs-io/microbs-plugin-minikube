62f2c00e8ab169f3c5ed96de188ea0042e92d5c7
Dave Moore <davemoore.cc@gmail.com>
2022-05-13 15:09:22 -0700

    Fixed bug in validations

--- src/validate.js
+++ src/validate.js
@@ -14,15 +14,15 @@ const { logger, utils } = require('@microbs.io/core')
  */
 const validateMinikubeInstallation = () => {
   if(commandExists.sync('minikube'))
-    return {
+    return [{
       success: true,
       message: 'minikube is installed'
-    }
+    }]
   else
-    return {
+    return [{
       success: true,
       message: 'minikube is not installed'
-    }
+    }]
 }
 
 /**
@@ -35,28 +35,28 @@ const validateMinikubeVersion = () => {
       versionActual = semver.clean(result.stdout.match(/minikube version: v(.+)/)[1])
       versionRequired = semver.clean('1.25.2')
       if (semver.gte(versionActual, versionRequired))
-        return {
+        return [{
           success: true,
           message: `minikube is correct version [using=${versionActual}, required>=${versionRequired}]`
-        }
+        }]
       else
-        return {
+        return [{
           success: false,
           message: `minikube is incorrect version [using=${versionActual}, required>=${versionRequired}]`
-        }
+        }]
     } catch (e) {
       logger.error(e)
-      return {
+      return [{
         success: false,
         message: e
-      }
+      }]
     }
   } else {
     logger.warn(result.stderr)
-    return {
+    return [{
       success: false,
       message: result.stderr
-    }
+    }]
   }
 }
 
